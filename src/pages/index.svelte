effb3f928a6e07bdcb89d975c3715895bfb6f5af
codelrond <codelrond@protonmail.com>
2022-04-08 13:11:27 +0200

    map.panTo() on click

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,11 +1,19 @@
 <script>
   import 'leaflet/dist/leaflet.css'
   import {LeafletMap, Marker, TileLayer} from 'svelte-leafletjs';
+import { handle_promise } from 'svelte/internal';
 
   const mapOptions = {
       center: [1.364917, 103.822872],
       zoom: 11,
   };
+  let MAP_EL;
+  function handle(ev){
+    let latlng = ev.detail.latlng;
+    console.log(latlng)
+    let map = MAP_EL.getMap();
+    map.panTo(latlng)
+  }
   const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
   const tileLayerOptions = {
       minZoom: 0,
@@ -16,7 +24,7 @@
 </script>
 
 <div class="example">
-  <LeafletMap options={mapOptions}>
+  <LeafletMap bind:this={MAP_EL} events={['click']}  on:click={handle} options={mapOptions}>
       <TileLayer url={tileUrl} options={tileLayerOptions}/>
   </LeafletMap>
 </div>
