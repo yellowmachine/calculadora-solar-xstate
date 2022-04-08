fceeb299a43a5e119cf9a1e713f862517b830045
codelrond <codelrond@protonmail.com>
2022-04-08 15:39:22 +0200

    map component in its own file

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,38 +1,6 @@
-<script>
-  import 'leaflet/dist/leaflet.css'
-  import {LeafletMap, Marker, TileLayer} from 'svelte-leafletjs';
-import { handle_promise } from 'svelte/internal';
-
-  const mapOptions = {
-      center: [1.364917, 103.822872],
-      zoom: 11,
-  };
-  let MAP_EL;
-  let latlng;
-
-  const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-  const tileLayerOptions = {
-      minZoom: 0,
-      maxZoom: 20,
-      maxNativeZoom: 19,
-      attribution: "© OpenStreetMap contributors",
-  };
-  $: {
-    if(MAP_EL && latlng){
-      let map = MAP_EL.getMap();
-      map.panTo(latlng)
-    }    
-  }
+<script>  
+  import Mapa from '../lib/Mapa.svelte'
+  let latlng={lat: 1.364917, lng: 103.822872,}
 </script>
 
-<div class="example">
-  <LeafletMap bind:this={MAP_EL} events={['click']}  on:click={(ev) => latlng = ev.detail.latlng} options={mapOptions}>
-      <TileLayer url={tileUrl} options={tileLayerOptions}/>
-  </LeafletMap>
-</div>
-
-<style>
-  .example{
-    height: 500px;
-  }
-</style>
+<Mapa bind:latlng={latlng} />
