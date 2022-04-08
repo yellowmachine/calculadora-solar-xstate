a25a0889771533d16bff8d1dfd74167dbe5f9d8c
codelrond <codelrond@protonmail.com>
2022-04-08 19:47:16 +0200

    adding bindings

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,6 +1,31 @@
 <script>  
   import Mapa from '../lib/Mapa.svelte'
-  let latlng={lat: 1.364917, lng: 103.822872,}
+  let lat = 1.364917;
+  let lng = 103.822872;
+  let latlng={lat, lng}
+
+  function isLatitude(v){
+    return true
+  }
+
+  function isLongitude(v){
+    return true
+  }
+
+  $: if(isLatitude(lat) && isLongitude(lng)) latlng = {lat, lng}
 </script>
 
+<div class="mb-4">
+  <label class="block text-gray-700 text-sm font-bold mb-2" for="latitude">
+    Latitud
+  </label>
+  <input bind:value={lat} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="latitude" type="text" placeholder="Latitud">
+</div>
+<div class="mb-4">
+  <label class="block text-gray-700 text-sm font-bold mb-2" for="longitude">
+    Longitud
+  </label>
+  <input bind:value={lng} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="longitude" type="text" placeholder="Longitud">
+</div>
+{JSON.stringify(latlng)}
 <Mapa bind:latlng={latlng} />
