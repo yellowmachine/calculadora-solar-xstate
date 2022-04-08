6be10db5d248371a2699b96a8326827d07e1c787
codelrond <codelrond@protonmail.com>
2022-04-08 12:22:26 +0200

    added leaflet map in index.svelte

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,22 +1,28 @@
-<script lang="ts">
-  import Counter from '../lib/Counter.svelte'
-  import Go from '../lib/Go.svelte'
+<script>
+  import 'leaflet/dist/leaflet.css'
+  import {LeafletMap, Marker, TileLayer} from 'svelte-leafletjs';
+
+  const mapOptions = {
+      center: [1.364917, 103.822872],
+      zoom: 11,
+  };
+  const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+  const tileLayerOptions = {
+      minZoom: 0,
+      maxZoom: 20,
+      maxNativeZoom: 19,
+      attribution: "© OpenStreetMap contributors",
+  };
 </script>
 
-<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-  Button
-</button>
+<div class="example">
+  <LeafletMap options={mapOptions}>
+      <TileLayer url={tileUrl} options={tileLayerOptions}/>
+  </LeafletMap>
+</div>
 
-<button class="btn btn-warning">Warning</button>
-
-<Counter />
-
-<br/>
-
-<Go />
-
-<br />
-
-<a href="/about">About</a>
-
-<br />
+<style>
+  .example{
+    height: 500px;
+  }
+</style>
