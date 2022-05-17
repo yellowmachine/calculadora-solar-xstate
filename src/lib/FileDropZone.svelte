005a04c26f96516a8483c26ca8aac37b16f19125
codelrond78 <codelrond@protonmail.com>
2022-05-17 19:51:47 +0200

    no more dropzone, error while building now using file input

--- src/lib/FileDropZone.svelte
+++ src/lib/FileDropZone.svelte
@@ -1,9 +1,10 @@
 <script>
-    import Dropzone from "svelte-file-dropzone";
+    //import Dropzone from "svelte-file-dropzone";
     import { csv2mapa, mapacsv2array } from './utils';
     import vector from './fechayhoracsv'
     import {isHeader, popWhile} from './utils';
-
+    
+    let files;
     function extractFromRawCSV(raw){
     if(raw === null){
         return {header: '', tail: ''}
@@ -15,20 +16,11 @@
     return {header: h, tail}
     }
   
-    let files = {
-      accepted: [],
-      rejected: []
-    };
-  
     let content;
     export let data;
 
-    function handleFilesSelect(e) {
-      const { acceptedFiles, fileRejections } = e.detail;
-      files.accepted = [...files.accepted, ...acceptedFiles];
-      files.rejected = [...files.rejected, ...fileRejections];
-
-      console.log('El fichero que se va a calcular es', files.accepted[0]);
+    function handleFilesSelect(file) {
+      console.log('El fichero que se va a calcular es', file);
       const reader = new FileReader();
   
       reader.onload = (evload) => {
@@ -38,17 +30,21 @@
           const arr = mapacsv2array(mapa, vector);
           data = arr;
       }
-      reader.readAsText(files.accepted[0]);
-
+      reader.readAsText(file);
     }
+
+  $: if(files && files[0]) handleFilesSelect(files[0])
   </script>
   
-  <Dropzone on:drop={handleFilesSelect} />
-  <ol>
-    {#each files.accepted as item}
-      <li>{item.name}</li>
-    {/each}
-  </ol>
+  <!--<Dropzone on:drop={handleFilesSelect} />-->
+  <input type="file" class="btn" bind:files>
+
+  {#if files && files[0]}
+    <p>
+      {files[0].name}
+    </p>
+  {/if}
+
   <div>
     <pre>{content.header}</pre>
     <pre>{content.tail}</pre>
