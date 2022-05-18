eb04ec24837048779760cfed70e445e9b5d97994
codelrond78 <codelrond@protonmail.com>
2022-05-18 08:40:52 +0200

    center grouper and margins

--- src/lib/Grouper.svelte
+++ src/lib/Grouper.svelte
@@ -6,7 +6,7 @@
     $: color = errors.some(x => x) ? "warning": "info"
 </script>
 
-<div>
+<div class="mb-2 mt-2 w-full grid grid-cols-1 place-items-center">
     <button class={`btn btn-${color} w-80`} on:click={()=>show=!show}>{label}</button>
     {#if show}
         <slot />
