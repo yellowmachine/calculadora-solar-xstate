f7c853f99b6ae1193136fbd9279d4adc1a20f76c
codelrond78 <codelrond@protonmail.com>
2022-05-18 11:26:35 +0200

    added axes y and x

--- src/pages/cake.svelte
+++ src/pages/cake.svelte
@@ -1,6 +1,8 @@
 <script>
     import { LayerCake, Svg } from 'layercake';
     import Scatter from '../lib/Scatter.svelte';
+    import AxisX from '../lib/AxisX.svelte';
+    import AxisY from '../lib/AxisY.svelte';
   
     const points = [
       {x: 0, y: 0},
@@ -33,6 +35,8 @@
       <Svg>
         <!-- You can expose properties on your chart components to make them more reusable -->
         <Scatter fill={'blue'} r={3} />
+        <AxisX />
+        <AxisY />
       </Svg>
     </LayerCake>
   </div>
